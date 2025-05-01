cd3cb67b68ab0a6db82ea121223e5a9bc8a3e430
GauravPrasad15 <gaurav.prasad.kol@gmail.com>
2025-05-01 12:58:45 +0530

    project structure ready, venv active, libraries installed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Gaurav Prasad",
+    author_email="gaurav.prasad.kol@gmail.com",
+    packages=find_packages()
+)
